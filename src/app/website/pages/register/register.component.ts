107b73d432c9111d96857002137b344a16f4ffee
Viviana9110 <v.londono.naranjo@gmail.com>
2022-05-02 16:05:50 -0500

    Se agrega el proyecto

---
+++ src/app/website/pages/register/register.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { OnExit } from './../../../guards/exit.guard';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit, OnExit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onExit(){
+    const rta = confirm('Logica desde comp, estas seguro salir?');
+    return rta;
+  }
+
+}
